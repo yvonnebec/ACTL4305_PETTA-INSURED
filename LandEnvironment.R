9d4c10caf3165c9da44358fde53eb6705c4754ef
Dylan Walz <d.walz@student.unsw.edu.au>
2024-10-15 13:58:46 +1100

    Including raw weighted land data

--- LandEnvironment.R
+++ LandEnvironment.R
@@ -63,27 +63,43 @@ result$weighted_national_parks <- result$weighted_national_parks / 100
 banded_result <- result %>%
   mutate(
     national_parks_band = cut(weighted_national_parks * 100,
-                              breaks = c(-Inf, 0, 5, 10, 25, 50, 75, 100),  # '-Inf' to handle exactly 0
-                              labels = c("0", "0-5%", "5-10%", "10-25%", "25-50%", "50-75%", "75-100%"),
+                              breaks = c(-Inf, 0, 5, 10, 25, 50, 75, 100),
+                              labels = c("0", "0-5%", "5-10%", "15-25%", "25-50%", "50-75%", "75-100%"),
+                              #breaks = c(0, 20, 40, 60, 80, 100),
+                              #labels = c("0-20%", "20-40%", "40-60%", "60-80%", "80-100%"),
                               include.lowest = TRUE,
                               right = TRUE),
     agricultural_land_band = cut(weighted_agricultural_land,
-                                 breaks = c(-Inf, 0, 10000, 100000, 1000000, 10000000, Inf),  # '-Inf' for exactly 0
+                                 breaks = c(-Inf, 0, 10000, 100000, 1000000, 10000000, Inf),
                                  labels = c("0", "0-10,000", "10,000-100,000", "100,000-1,000,000", "1,000,000-10,000,000", "10,000,000+"),
                                  include.lowest = TRUE,
                                  right = TRUE)
-  ) %>% select(POSTCODE, national_parks_band, agricultural_land_band)
+  )
 
 severity_total <- read.csv("severity_total.csv")
-View(severity_total)
 
 severity_total <- severity_total %>%
+  select(-national_parks_band, -agricultural_land_band) %>% 
   left_join(banded_result, by = c("nb_postcode" = "POSTCODE")) %>% 
+  rename(agricultural_land = weighted_agricultural_land,
+         national_parks = weighted_national_parks)
+
+colnames(severity_total)
+
+severity_total <- severity_total %>% 
   mutate(
+    agricultural_land = as.character(agricultural_land),
+    national_parks = as.character(national_parks),
+    agricultural_land_band = as.character(agricultural_land_band),
+    national_parks_band = as.character(national_parks_band)
+  ) %>%
+  mutate(
+    agricultural_land = replace_na(agricultural_land, "Unknown"),
+    national_parks = replace_na(national_parks, "Unknown"),
     agricultural_land_band = replace_na(agricultural_land_band, "Unknown"),
     national_parks_band = replace_na(national_parks_band, "Unknown")
   )
 
-
+table(is.na(severity_total$agricultural_land_band))
 
 write.csv(severity_total, file = "severity_total.csv", row.names = FALSE)
