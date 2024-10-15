2f76a9eccdf6f6a6bcf84cbd3b74e0f9eb83142c
Dylan Walz <d.walz@student.unsw.edu.au>
2024-10-15 12:42:01 +1100

    Updating LandEnvironment R script

--- LandEnvironment.R
+++ LandEnvironment.R
@@ -1,3 +1,6 @@
+# Import Lib
+library(tidyverse)
+
 # Import data frames
 land_environment <- read.csv("EXTERNAL_LANDENVIRONMENT.csv", skip = 6, header = FALSE)
 SA4 <- read.csv("postcode_SA4.csv")
@@ -19,9 +22,7 @@ land_environment_np <- land_environment %>%
 
 # National Park % data and Postcode (2022 data)
 merged_data_np <- land_environment_np %>%
-  left_join(SA4, by = c("Code" = "SA4_CODE_2011")) %>%
-  select(POSTCODE, National_parks)
-
+  left_join(SA4, by = c("Code" = "SA4_CODE_2011"))
 
 # Area of agricultural land (ha) 2021
 land_environment_al <- land_environment %>%
@@ -29,40 +30,54 @@ land_environment_al <- land_environment %>%
   rename("Agriculture_land" = "Area of agricultural land (ha)")
 
 merged_data_al <- land_environment_al %>%
-  left_join(SA4, by = c("Code" = "SA4_CODE_2011")) %>%
-  select(POSTCODE, Agriculture_land)
+  left_join(SA4, by = c("Code" = "SA4_CODE_2011"))
 
-land_data <- merge(merged_data_al, merged_data_np, by = "POSTCODE")
-land_data <- land_data %>% mutate_all(~replace(., . == "-", "Unknown"))
-land_data %>% filter(National_parks == "Unknown")
+land_data <- merge(merged_data_al, merged_data_np, by = "Code")
 
-# Convert Agriculture_land to numeric and National_parks to numeric
 land_data <- land_data %>%
+  select(-matches("\\.y$"), -Year.x)
+
+colnames(land_data) <- gsub("\\.x$", "", colnames(land_data))
+
+land_data <- land_data %>% distinct()
+
+merged_data <- land_data %>%
   mutate(
+    PERCENTAGE = as.numeric(PERCENTAGE) / 100,
     Agriculture_land = as.numeric(gsub(",", "", Agriculture_land)),
-    National_parks = as.numeric(National_parks)
+    National_parks = as.numeric(National_parks),
+    weighted_national_parks = National_parks * PERCENTAGE,
+    weighted_agricultural_land = Agriculture_land * PERCENTAGE,
   )
 
-# Band land variables
-land_data_banded <- land_data %>%
+result <- merged_data %>%
+  group_by(POSTCODE) %>%
+  summarise(
+    weighted_national_parks = sum(weighted_national_parks, na.rm = TRUE),
+    weighted_agricultural_land = sum(weighted_agricultural_land, na.rm = TRUE),
+  )
+
+result$weighted_national_parks <- result$weighted_national_parks / 100
+
+# Banding land data
+banded_result <- result %>%
   mutate(
-    Agriculture_land_band = case_when(
-      is.na(Agriculture_land) ~ "Unknown",
-      TRUE ~ cut(as.numeric(gsub(",", "", Agriculture_land)),
-                 breaks = c(0, 10000, 50000, 100000, 1000000, 10000000, Inf),
-                 labels = c("0-10,000 ha", "10,000-50,000 ha", "50,000-100,000 ha",
-                            "100,000-1,000,000 ha", "1,000,000-10,000,000 ha", "10,000,000+ ha"),
-                 include.lowest = TRUE)
-    ),
-    National_parks_band = case_when(
-      is.na(National_parks) ~ "Unknown",
-      TRUE ~ cut(National_parks,
-                 breaks = c(0, 10, 25, 50, 75, 100),
-                 labels = c("0-10%", "10-25%", "25-50%", "50-75%", "75-100%"),
-                 include.lowest = TRUE)
-    )
-  )
+    national_parks_band = cut(weighted_national_parks * 100,
+                              breaks = c(-Inf, 0, 5, 10, 25, 50, 75, 100),  # '-Inf' to handle exactly 0
+                              labels = c("0", "0-5%", "5-10%", "10-25%", "25-50%", "50-75%", "75-100%"),
+                              include.lowest = TRUE,
+                              right = TRUE),
+    agricultural_land_band = cut(weighted_agricultural_land,
+                                 breaks = c(-Inf, 0, 10000, 100000, 1000000, 10000000, Inf),  # '-Inf' for exactly 0
+                                 labels = c("0", "0-10,000", "10,000-100,000", "100,000-1,000,000", "1,000,000-10,000,000", "10,000,000+"),
+                                 include.lowest = TRUE,
+                                 right = TRUE)
+  ) %>% select(POSTCODE, national_parks_band, agricultural_land_band)
 
-land_data_banded %>% select(POSTCODE, Agriculture_land_band, National_parks_band)
-write.csv(land_data_banded, file = "environment_land_data.csv", row.names = FALSE)
+severity_total <- read.csv("severity_total.csv")
+View(severity_total)
 
+severity_total <- severity_total %>%
+  left_join(banded_result, by = c("nb_postcode" = "POSTCODE"))
+
+write.csv(severity_total, file = "severity_total.csv", row.names = FALSE)
